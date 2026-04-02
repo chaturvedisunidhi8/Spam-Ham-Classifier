a792d67325f81fc26ce657de8636274108d18296
Sunidhi Chaturvedi <chaturvedisunidhi8@gmail.com>
2026-04-02 15:20:03 +0530

    Update Using Bert ARCHITECTURE.py

--- Using Bert ARCHITECTURE.py
+++ Using Bert ARCHITECTURE.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorflow_hub as hub  
-import tensorflow_text as text
+import tensorflow_text as text 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics.pairwise import cosine_similarity
